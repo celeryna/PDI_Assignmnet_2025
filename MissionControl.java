cab435d0fe093f50d16559391ea3afbe63d3157b
Cel Reyes <reyescelerinacr13@gmail.com>
2025-04-17 13:53:20 +0800

    feature: Add classes for managing, missions and atronauts

---
+++ MissionControl.java
@@ -0,0 +1,21 @@
+/************************************
+ * Author: Celerina Reyes           *
+ * Created: 07/10/2024              *
+ * Purpose: Contains Main Menu      *
+ ************************************/
+
+import java.util.*;
+
+public class MissionControl
+{
+    // METHOD: main
+    // IMPORT: args (String[]), command-line arguments
+    // EXPORT none
+    // ASSERTION: THe entry point for the application
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+    }
+
+
+}
---
+++ MissionManager.java
@@ -0,0 +1,13 @@
+/******************************************
+ * Author: Celerina Reyes                 *
+ * Created: 17/04/2025                    *
+ * Purpose: Manages the system's missions *
+ *****************************************/
+
+import java.io.*;
+
+public class MissionManager
+{
+    public String[][] missionsData; // A 2D array to store raw mission data from the CVS file      
+    public Mission[] missions; // An array to store mission obects after processing the data from the CVS file
+}
